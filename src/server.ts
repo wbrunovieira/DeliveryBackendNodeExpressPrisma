ec606b509500b92298e767c40942387ae762f9e1
wbrunovieira <wbrunovieira77@gmail.com>
2022-02-17 09:25:26 +0000

    added express-async-errors to the project

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,22 @@
-import express, { NextFunction, Request, Response } from 'express';
-// import 'express-async-errors';
+import express, { NextFunction, Request, Response, ErrorRequestHandler, Errback } from 'express';
+import 'express-async-errors';
+
 import { routes } from './routes';
 
 const app = express();
 
 app.use(express.json());
+
+const errorHandling = (err:Error, request:Request, response:Response, next:NextFunction) => {
+  response.status(500).json({
+    msg: err.message,
+    success: false,
+  });
+};
+
 app.use(routes);
+app.use(errorHandling);
 
-// app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
-//   if (err instanceof Error) {
-//     return response.status(400).json({
-//       message: err.message,
-//     });
-//   }
-
-//   return response.status(500).json({
-//     status: 'error',
-//     message: 'Internal server error',
-//   });
-// });
 
 app.get('/', (request, response) => {
   return response.json({
